9ec125a6e08d0452e8d50ef092e24d706083f556
Alex Chan <alex@AlexMacBookPro.local>
2022-03-07 18:26:15 +0000

    emailer

--- manager/emailer.py
+++ manager/emailer.py
@@ -16,21 +16,30 @@ def sendOutingReminders(outing_id):
     outing = Outing.objects.get(id=outing_id)
     crsids = []
 
-    body = """<html>
+    body = (
+        """<html>
               <head></head>
               <body>
               <p>Dear rower/cox/coach,</p>
-              <p>You have an outing coming up on """ + str(outing.date) + " at " + str(outing.meetingTime) + """.</p>
+              <p>You have an outing coming up on """
+        + str(outing.date)
+        + " at "
+        + str(outing.meetingTime)
+        + """.</p>
               <p>You can check your upcoming outings at http://secbc.herokuapp.com/myOutings/</p>
               <p>/SECBC Website</p>
               </body></html>
               """
+    )
 
     # Todo can be made more robust
     for inOuting in InOuting.objects.filter(outing=outing):
         person = inOuting.person
 
-        if (HasBeenMailedOuting.objects.filter(person=person, outing=outing).count() == 0):
+        if (
+            HasBeenMailedOuting.objects.filter(person=person, outing=outing).count()
+            == 0
+        ):
             crsids += [person.username]
             HasBeenMailedOuting(person=person, outing=outing).save()
 
@@ -40,7 +49,7 @@ def sendOutingReminders(outing_id):
 # Todo Use Django email api instead
 def send_email(crsids, subject, body):
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        sent_from = 'secbc.web@gmail.com'
+        sent_from = "secbc.web@gmail.com"
         server.login(sent_from, email_password)
         i = True
         for email in [crsid + "@cam.ac.uk" for crsid in crsids]:
@@ -50,37 +59,43 @@ def send_email(crsids, subject, body):
                 "Subject: " + subject,
                 "To: " + email,
                 "MIME-Version: 1.0",
-                "Content-Type: text/html"]
+                "Content-Type: text/html",
+            ]
             if i:
-                headers += ['CC: stedmunds.captain@cucbc.org']
+                headers += ["CC: stedmunds.captain@cucbc.org"]
                 i = False
             headers = "\r\n".join(headers)
             server.sendmail(sent_from, send_to, headers + "\r\n\r\n" + body)
 
 
 def sendSignupDetails(crsid, password):
-    sent_from = 'secbc.web@gmail.com'
+    sent_from = "wolfson.captain@cucbc.org"
     send_to = crsid + "@cam.ac.uk"
-    subject = 'Welcome to St. Edmunds College Boat Club'
+    subject = "Welcome to Wolfson College Boat Club"
 
     headers = [
         "From: " + sent_from,
         "Subject: " + subject,
         "To: " + send_to,
         "MIME-Version: 1.0",
-        "Content-Type: text/html"]
+        "Content-Type: text/html",
+    ]
     headers = "\r\n".join(headers)
 
-    body = """<html>
+    body = (
+        """<html>
       <head></head>
       <body>
-      <p>Welcome to the SECBC website! Sign in at http://secbc.herokuapp.com using """ + password + """ as your password.</p>
+      <p>Welcome to the SECBC website! Sign in at http://secbc.herokuapp.com using """
+        + password
+        + """ as your password.</p>
       <p>Please remember to change your password after you've logged in</p>
       <p>HOW TO SIGN UP FOR ERG SESSIONS: Go to login -> Sign up for outings as a rower -> Click 'join' on any erg times for which you may be available</p>
       <p>Once you've marked yourself as available, the captains may assign you a spot on any of your available days.</p>
       <p>TO SEE IF YOU'VE BEEN ALLOTED FOR AN ERG SESSION: Check 'view my outings' a bit later (We'll also be sending out mails).</p>
       </body></html>
       """
+    )
 
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(sent_from, email_password)
--- rowingManager/settings.py
+++ rowingManager/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '_-i*z-todc5-kd=g-mz0iul*!j@x9l)wbpt6epgligixbu!n&4'
-SECRET_KEY = os.environ.get('SECRET_KEY','Optional default value')
+# SECRET_KEY = '_-i*z-todc5-kd=g-mz0iul*!j@x9l)wbpt6epgligixbu!n&4'
+SECRET_KEY = os.environ.get("SECRET_KEY", "Optional default value")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -36,59 +36,59 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'manager'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "manager",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'rowingManager.urls'
+ROOT_URLCONF = "rowingManager.urls"
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'secbc.web@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASSWORD')
+EMAIL_HOST_USER = "wolfson.captain@cucbc.org"
+EMAIL_HOST_PASSWORD = os.environ.get("GMAIL_PASSWORD")
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'rowingManager.wsgi.application'
+WSGI_APPLICATION = "rowingManager.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -98,16 +98,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -115,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -129,5 +129,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 django_heroku.settings(locals())
